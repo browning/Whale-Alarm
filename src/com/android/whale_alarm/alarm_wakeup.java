741f58de1623468bd5a1426940c4b5544f90e919
unknown <brian@.gdsprod.com>
2010-11-03 08:01:40 +0100

    wakeup device

--- src/com/android/whale_alarm/alarm_wakeup.java
+++ src/com/android/whale_alarm/alarm_wakeup.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.util.*;
 import android.widget.Toast;
 
@@ -16,10 +17,25 @@ public class alarm_wakeup extends BroadcastReceiver
 	public void onReceive(Context context, Intent intent) 
 	{
 		Log.e("testing", "hit it!!!!!!");
+		
+		PowerManager.WakeLock sCpuWakeLock;
+		
+       
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+		
+		sCpuWakeLock = pm.newWakeLock(
+	               PowerManager.PARTIAL_WAKE_LOCK |
+		                 PowerManager.ACQUIRE_CAUSES_WAKEUP |
+		               PowerManager.ON_AFTER_RELEASE, "blah");
+        sCpuWakeLock.acquire();
+		
 		Intent alarmView = new Intent(context, alarm_on.class);
 		intent.putExtra("go_off", true);
 		
 		alarmView.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(alarmView);
+	
+		sCpuWakeLock.release();
+		sCpuWakeLock = null;
 	}
 }
